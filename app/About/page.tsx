36a906fa6688f5ce0226135a8dec35c3b15c5b9b
SydneyAntoni <sydney.antoni@xapling.de>
2024-08-21 21:33:19 +0200

    Added different OGPs for diffrent subsites and some formatting

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,39 +1,51 @@
 import Image from "next/image";
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
 export default function Home() {
-    return (
-        <main>
-            <h1>About</h1>
-            <div className={styles.imageTextContainer}>
-                <div>
-                    <h2 className={styles.subheading}>
-                        Subheading:
-                    </h2>
-                    <p>
-                        Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite
-                        perfect nostrud nisi intricate Content. Qui  international first-class nulla ut. Punctual
-                        adipisicing, essential lovely queen tempor eiusmod irure. Exclusive izakaya charming Scandinavian
-                        impeccable aute quality of life soft power pariatur Melbourne occaecat discerning. Qui wardrobe
-                        aliquip, et Porter destination Toto remarkable officia Helsinki excepteur Basset hound. Zürich
-                        sleepy perfect consectetur.
-                        Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite
-                        perfect nostrud nisi intricate Content. Qui  international first-class nulla ut. Punctual
-                        adipisicing, essential lovely queen tempor eiusmod irure. Exclusive izakaya charming Scandinavian
-                        impeccable aute quality of life soft power pariatur Melbourne occaecat discerning. Qui wardrobe
-                        aliquip, et Porter destination Toto remarkable officia Helsinki excepteur Basset hound. Zürich
-                        sleepy perfect consectetur.
-                    </p>
-                </div>
-                <div className={styles.imageContainer}>
-                    <Image
-                        src={process.env.NEXT_PUBLIC_BASE_URL + '/portrait.png'}
-                        alt={'Portrait'}
-                        width={400}
-                        height={450}
-                    />
-                </div>
-            </div>
-        </main>
-    )
+  return (
+    <>
+      <head>
+        <meta property="og:title" content="About - Sydney Antoni" />
+        <meta
+          property="og:description"
+          content="Portfolio Website for Sydney Antoni (About Page)"
+        />
+      </head>
+      <main>
+        <h1>About</h1>
+        <div className={styles.imageTextContainer}>
+          <div>
+            <h2 className={styles.subheading}>Subheading:</h2>
+            <p>
+              Excepteur efficient emerging, minim veniam anim aute carefully
+              curated Ginza conversation exquisite perfect nostrud nisi
+              intricate Content. Qui international first-class nulla ut.
+              Punctual adipisicing, essential lovely queen tempor eiusmod irure.
+              Exclusive izakaya charming Scandinavian impeccable aute quality of
+              life soft power pariatur Melbourne occaecat discerning. Qui
+              wardrobe aliquip, et Porter destination Toto remarkable officia
+              Helsinki excepteur Basset hound. Zürich sleepy perfect
+              consectetur. Excepteur efficient emerging, minim veniam anim aute
+              carefully curated Ginza conversation exquisite perfect nostrud
+              nisi intricate Content. Qui international first-class nulla ut.
+              Punctual adipisicing, essential lovely queen tempor eiusmod irure.
+              Exclusive izakaya charming Scandinavian impeccable aute quality of
+              life soft power pariatur Melbourne occaecat discerning. Qui
+              wardrobe aliquip, et Porter destination Toto remarkable officia
+              Helsinki excepteur Basset hound. Zürich sleepy perfect
+              consectetur.
+            </p>
+          </div>
+          <div className={styles.imageContainer}>
+            <Image
+              src={process.env.NEXT_PUBLIC_BASE_URL + "/portrait.png"}
+              alt={"Portrait"}
+              width={400}
+              height={450}
+            />
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import styles from './layout.module.css'
+import styles from "./layout.module.css";
 import Image from "next/image";
 import Link from "@/app/components/Link/Link";
 
@@ -18,86 +18,111 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <head>
-      <title>Portfolio - Sydney Antoni</title>
-      <link rel="apple-touch-icon" sizes="180x180"
-            href={process.env.NEXT_PUBLIC_BASE_URL + "/apple-touch-icon.png"}/>
-      <link rel="icon" type="image/png" sizes="32x32"
-            href={process.env.NEXT_PUBLIC_BASE_URL + "/favicon-32x32.png"}/>
-      <link rel="icon" type="image/png" sizes="16x16"
-            href={process.env.NEXT_PUBLIC_BASE_URL + "/favicon-16x16.png"}/>
-      <link rel="manifest" href={process.env.NEXT_PUBLIC_BASE_URL + "/site.webmanifest"}/>
-      <link rel="mask-icon" href={process.env.NEXT_PUBLIC_BASE_URL + "/safari-pinned-tab.svg"} color="#2b8eab"/>
-      <link rel="shortcut icon" href={process.env.NEXT_PUBLIC_BASE_URL + "/favicon.ico"}/>
-      <meta name="msapplication-TileColor" content="#2b5797"/>
-      <meta name="msapplication-config" content={process.env.NEXT_PUBLIC_BASE_URL + "/browserconfig.xml"}/>
-      <meta name="theme-color" content="#ffffff"/>
-      <meta property="og:title" content="Portfolio - Sydney Antoni"/>
-      <meta property="og:description" content="Portfolio Website for Sydney Antoni"/>
-      <meta property="og:url" content={process.env.NEXT_PUBLIC_BASE_URL}/>
-      <meta property="og:image" content={process.env.NEXT_PUBLIC_BASE_URL + "/portrait.png"}/>
-      <meta property="og:image:alt" content="A Portrait of Sydney Antoni"/>
-      <meta property="og:type" content="website"/>
-    </head>
-    <body className={inter.className}>
-    <header>
-      <div className={styles.containerBalken}>
-        <div className={styles.magentaBalken}></div>
-        <div className={styles.orangeBalken}></div>
-      </div>
-      <div className={styles.containerLogoNav}>
-        <button className={styles.buttonLogo}>
-          <a href={'/'} className={styles.buttonLogoLink}>
-            <Image
-                src={process.env.NEXT_PUBLIC_BASE_URL + '/logo.svg'}
-                alt="logo"
-                width="50"
-                height="50"
-            />
-          </a>
-        </button>
-        <nav>
-          <ul className={styles.containerNav}>
-            <li className={styles.itemNav}>
-              <Link link={'/'}>Home</Link>
-            </li>
-            <li className={styles.itemNav}>
-            <Link link={'/Projects'}>Projects</Link>
-              </li>
-              <li className={styles.itemNav}>
-                <Link link={'/About'}>About</Link>
-              </li>
-              <li className={styles.itemNav}>
-                <Link link={'/Contact'}>Contact</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
+      <head>
+        <title>Portfolio - Sydney Antoni</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={process.env.NEXT_PUBLIC_BASE_URL + "/apple-touch-icon.png"}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={process.env.NEXT_PUBLIC_BASE_URL + "/favicon-32x32.png"}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={process.env.NEXT_PUBLIC_BASE_URL + "/favicon-16x16.png"}
+        />
+        <link
+          rel="manifest"
+          href={process.env.NEXT_PUBLIC_BASE_URL + "/site.webmanifest"}
+        />
+        <link
+          rel="mask-icon"
+          href={process.env.NEXT_PUBLIC_BASE_URL + "/safari-pinned-tab.svg"}
+          color="#2b8eab"
+        />
+        <link
+          rel="shortcut icon"
+          href={process.env.NEXT_PUBLIC_BASE_URL + "/favicon.ico"}
+        />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta
+          name="msapplication-config"
+          content={process.env.NEXT_PUBLIC_BASE_URL + "/browserconfig.xml"}
+        />
+        <meta name="theme-color" content="#ffffff" />
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_BASE_URL} />
+        <meta
+          property="og:image"
+          content={process.env.NEXT_PUBLIC_BASE_URL + "/portrait.png"}
+        />
+        <meta property="og:image:alt" content="A Portrait of Sydney Antoni" />
+        <meta property="og:type" content="website" />
+      </head>
+      <body className={inter.className}>
+        <header>
+          <div className={styles.containerBalken}>
+            <div className={styles.magentaBalken}></div>
+            <div className={styles.orangeBalken}></div>
+          </div>
+          <div className={styles.containerLogoNav}>
+            <button className={styles.buttonLogo}>
+              <a href={"/"} className={styles.buttonLogoLink}>
+                <Image
+                  src={process.env.NEXT_PUBLIC_BASE_URL + "/logo.svg"}
+                  alt="logo"
+                  width="50"
+                  height="50"
+                />
+              </a>
+            </button>
+            <nav>
+              <ul className={styles.containerNav}>
+                <li className={styles.itemNav}>
+                  <Link link={"/"}>Home</Link>
+                </li>
+                <li className={styles.itemNav}>
+                  <Link link={"/Projects"}>Projects</Link>
+                </li>
+                <li className={styles.itemNav}>
+                  <Link link={"/About"}>About</Link>
+                </li>
+                <li className={styles.itemNav}>
+                  <Link link={"/Contact"}>Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
 
-      {children}
-      <footer style={{margin: 0}}>
-        <ul className={styles.listFooter}>
-          <li className={styles.listElements}>
-            <Image
-                src={process.env.NEXT_PUBLIC_BASE_URL + '/gitHub.svg'}
-                alt={'GitHub Logo'}
+        {children}
+        <footer style={{ margin: 0 }}>
+          <ul className={styles.listFooter}>
+            <li className={styles.listElements}>
+              <Image
+                src={process.env.NEXT_PUBLIC_BASE_URL + "/gitHub.svg"}
+                alt={"GitHub Logo"}
                 width={28}
                 height={28}
-            />
-          </li>
-          <li className={styles.listElements}>
-            <Image
-                src={process.env.NEXT_PUBLIC_BASE_URL + '/linkedIn.svg'}
-                alt={'LinkedIn Logo'}
+              />
+            </li>
+            <li className={styles.listElements}>
+              <Image
+                src={process.env.NEXT_PUBLIC_BASE_URL + "/linkedIn.svg"}
+                alt={"LinkedIn Logo"}
                 width={28}
                 height={28}
-            />
-          </li>
-          <li className={styles.listElements}>Impressum</li>
-          <li className={styles.listElements}>Datenschutz</li>
-        </ul>
-      </footer>
+              />
+            </li>
+            <li className={styles.listElements}>Impressum</li>
+            <li className={styles.listElements}>Datenschutz</li>
+          </ul>
+        </footer>
       </body>
     </html>
   );
